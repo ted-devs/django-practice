138c5ddd40936325c1fb9066a2523b4f2032eb83
Talha Salman <talha.salman22@gmail.com>
2024-02-08 00:04:54 +0500

    Added Models, Migrations and Forms

--- hello/views.py
+++ hello/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from typing import Any
+from django.shortcuts import render, redirect
 from django.utils.timezone import datetime
-import re
+from django.views.generic import ListView
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
 
 
 # Create your views here.
+
+
+class HomeListView(ListView):
+    """Renders the home page, with a list of all messages."""
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
 
 
 def home(request):
@@ -30,3 +41,15 @@ def hello_there(request, name):
             'date': datetime.now()
         }
     )
+
+
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        message = form.save(commit=False)
+        message.log_date = datetime.now()
+        message.save()
+        return redirect('home')
+    else:
+        return render(request, 'hello/log_message.html', {"form": form})
