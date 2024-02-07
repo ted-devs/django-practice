4a7d801eef71dd54652beac7cd1b78d3ec0b7fb5
Talha Salman <talha.salman22@gmail.com>
2024-02-07 23:00:28 +0500

    template is used to render html

--- hello/views.py
+++ hello/views.py
@@ -8,7 +8,15 @@ import re
 
 
 def home(request):
-    return HttpResponse('Hello, Django!')
+    return render(request, 'hello/home.html')
+
+
+def about(request):
+    return render(request, 'hello/about.html')
+
+
+def contact(request):
+    return render(request, 'hello/contact.html')
 
 
 def hello_there(request, name):
