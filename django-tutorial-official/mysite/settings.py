57b2f179f5531376625d0350234af03aa42385c8
Talha <talha.salman22@gmail.com>
2024-02-10 00:43:49 +0500

    Project created in root instead of subfolder.

--- django-tutorial-official/mysite/settings.py
+++ django-tutorial-official/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h0ys7gy921b_u@mf_ug&8&x0s=dm_fql%h#2q)gpo6)9i6a!4('
+SECRET_KEY = 'django-insecure-&@xdz!+b8h)v0oq%=$bnn@%4ld6fnp)ya)9rg!i9-hnt)im273'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
